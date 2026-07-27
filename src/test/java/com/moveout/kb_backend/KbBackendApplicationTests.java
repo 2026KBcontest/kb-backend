b9f4c5e9eec431f20f6b692a21e8c1b20e277666
itsmeyji <jungyunji031023@gmail.com>
2026-07-27 17:47:37 +0900

    init

---
+++ src/test/java/com/moveout/kb_backend/KbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moveout.kb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
